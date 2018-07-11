e3b7122c137a05feeeebfbc2ccf709664b8d0a08
Minki Ju <minkiju@outlook.com>
2018-07-11 12:31:17 +0900

    Update GUI resource and code

--- usicMusic/MainWindow.xaml.cs
+++ usicMusic/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace usicMusic
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class MainWindow : Window
     {
-        RecordWithWaveIn wave = new RecordWithWaveIn();
-        private Boolean state = false;
-
         public static string GetPath()
         {
             // 컴터마다 파일경로 다를거같아서 일단만들어놓음
@@ -22,61 +18,81 @@ namespace usicMusic
         public MainWindow()
         {
             InitializeComponent();
-            MouseLeftButtonDown += delegate { DragMove(); };
+            ApplicationBorder.MouseLeftButtonDown += delegate { DragMove(); };
         }
 
-        private void btnExit_Click(object sender, RoutedEventArgs e)
+        public static void ChangeSource(Image image, ImageSource source, TimeSpan fadeOutTime, TimeSpan fadeInTime)
+        {
+            var fadeInAnimation = new DoubleAnimation(1d, fadeInTime);
+
+            if (image.Source != null)
+            {
+                var fadeOutAnimation = new DoubleAnimation(0d, fadeOutTime);
+
+                fadeOutAnimation.Completed += (o, e) =>
+                {
+                    image.Source = source;
+                    image.BeginAnimation(Image.OpacityProperty, fadeInAnimation);
+                };
+
+                image.BeginAnimation(Image.OpacityProperty, fadeOutAnimation);
+            }
+            else
+            {
+                image.Opacity = 0d;
+                image.Source = source;
+                image.BeginAnimation(Image.OpacityProperty, fadeInAnimation);
+            }
+        }
+
+        private void btnExit_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ChangeSource(btnExit, (ImageSource)new ImageSourceConverter()
+                .ConvertFrom(new Uri(@"pack://application:,,,/Resource/Buttons/ExitButtonHover.png")),
+                new TimeSpan(0), new TimeSpan(0, 0, 0, 0, 150));
+        }
+
+        private void btnExit_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ChangeSource(btnExit, (ImageSource)new ImageSourceConverter()
+                .ConvertFrom(new Uri(@"pack://application:,,,/Resource/Buttons/ExitButton.png")),
+                new TimeSpan(0, 0, 0, 0, 150), new TimeSpan(0));
+        }
+
+        private void btnExit_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            btnExit.Source = (ImageSource)new ImageSourceConverter()
+                .ConvertFrom(new Uri(@"pack://application:,,,/Resource/Buttons/ExitButtonDown.png"));
+        }
+
+        private void btnExit_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnMinimize_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            string str = sender.ToString().Replace("System.Windows.Controls.Button: ", string.Empty);
-            if (!state)
-            {
-                Button(str);
-            }
-            else
-            {
-                StartMusic startMusic = new StartMusic(str);
-                startMusic.MusicStart();
-                // 노래틀어야됨 여기서 str 값에 따라서
-            }
+            ChangeSource(btnMinimize, (ImageSource)new ImageSourceConverter()
+               .ConvertFrom(new Uri(@"pack://application:,,,/Resource/Buttons/MinButtonHover.png")),
+               new TimeSpan(0), new TimeSpan(0, 0, 0, 0, 150));
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void btnMinimize_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            state = !state;
-            if (state)
-            {
-                IsExist Exist = new IsExist();
-                    
-                if(Exist.FiveExists()) // 파일개수가 5개가 아니면.
-                {
-                    state = !state;
-                    MessageBox.Show("파일 부족함.");
-                    return;
-                }
-                wave.StartRecord("ex01");
-                StartAndStop.Content = "stop";
-            }
-            else
-            {
-                SaveFileDialog sfd = new SaveFileDialog();
-                sfd.FileName = DateTime.Now.ToShortDateString() + " "
-                    + DateTime.Now.ToShortTimeString().Replace(":", "시 ") + "분";
-                sfd.Filter = "오디오 녹음|*.wav";
-                sfd.ShowDialog();
-
-                StartAndStop.Content = "start";
-            }
+            ChangeSource(btnMinimize, (ImageSource)new ImageSourceConverter()
+               .ConvertFrom(new Uri(@"pack://application:,,,/Resource/Buttons/MinButton.png")),
+               new TimeSpan(0, 0, 0, 0, 150), new TimeSpan(0));
         }
 
-        private void Button(String str)
+        private void btnMinimize_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            RecordOrLoad rl = new RecordOrLoad(str);
-            rl.Show();
+            btnMinimize.Source = (ImageSource)new ImageSourceConverter()
+                .ConvertFrom(new Uri(@"pack://application:,,,/Resource/Buttons/MinButtonDown.png"));
+        }
+
+        private void btnMinimize_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
         }
     }
 }
