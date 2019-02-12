09c3eba1cd12bd8e72a66e68b0303337a2d0dc3b
Hyo-Seong <dlgytjd0907@naver.com>
2019-02-12 23:55:48 +0900

    change view -> controls

--- usicMusic/Controls/LoopStation.xaml.cs
+++ usicMusic/Controls/LoopStation.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace usicMusic.View
+namespace usicMusic.Controls
 {
     /// <summary>
     /// Interaction logic for LoopStation.xaml
